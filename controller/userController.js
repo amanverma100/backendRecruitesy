a239142ca256e832758761618681a2b04d948756
Aman Verma <amanverma740895@gmail.com>
2025-05-20 01:05:16 +0530

    changig cookie sameSite

--- controller/userController.js
+++ controller/userController.js
@@ -178,7 +178,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+         secure: true, 
+        sameSite: "None"
       })
       .json({
         message: `Welcome back ${user.fullname}`,
